0d7573cd9a84018b211d799f525263d37cc656b2
kaliber64 <45779233+kaliber64@users.noreply.github.com>
2019-09-10 19:35:16 -0400

    [MMLX-21] redux (#127)
    * Zenny Implemented,1.0 Megaman Refactor with particles,C++ Project Conversion COmplete,Additional Events for Dynamic Targeting, Zenny Pickup Generalized,Reaverbot Damage Dealing Generalized.
    
    * Add HitBox Tag to Reaverbot Capsule. Generalize BusterShot check for Reaverbot Capsule
    
    * Add invulnerability after knockdown

---
+++ MMLX 4.22/Source/MMLX/MMLX.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MMLX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MMLX, "MMLX" );
